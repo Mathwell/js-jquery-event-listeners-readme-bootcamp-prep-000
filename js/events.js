f2f45cb77035defb6df16d96192214065cd45484
Olena Ageyeva <eageeva@hotmail.com>
2017-09-16 03:25:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -28,5 +28,23 @@ function pressIt(){
 $(document).ready(function(){
 
 // call functions here
-
 });
+
+function getIt(){
+  $('p').on('click', function() {alert("Hey!")})
+}
+
+function frameIt(){
+$(".tasty").on('load',function(){
+   $(this).css('border', "solid 2px red");
+  //$('.tasty').append('<frame cols="#FF0000" /frame>')
+})
+}
+
+function pressIt(){
+$("form").on('keydown', function(key){ if (key.which===71) alert("G was pressed!") })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){alert("Your form is going to be submitted now.")})
+}
